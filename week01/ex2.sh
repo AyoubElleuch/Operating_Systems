790af8aea4253ad2b98dd4bc3f8f238acac3c453
Mohamed Ayoub Eleuch <58662566+AyoubElleuch@users.noreply.github.com>
2023-09-10 17:56:43 +0400

    Add files via upload

---
+++ week01/ex2.sh
@@ -0,0 +1,4 @@
+cowsay "Hello, I am just a cow"
+cmatrix
+asciiquarium
+history > ex2.txt
---
+++ week01/ex3.sh
@@ -0,0 +1,8 @@
+mkdir root
+date & sleep 3
+ls -latr / > root/root.txt
+date & sleep 3
+mkdir home
+date & sleep 3
+ls -latr ~ > home/home.txt
+
